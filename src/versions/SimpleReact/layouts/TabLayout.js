14876e1def6f15b1e3bb97a169a5b11aa5841a97
Steven Rico <steven@intuitiveprogrammer.com>
2019-01-24 11:05:32 +0000

    Created SavedAdvice and tested route. Created components/ui.

--- src/versions/SimpleReact/layouts/TabLayout.js
+++ src/versions/SimpleReact/layouts/TabLayout.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 
-import Tab from '../components/Tab'
+import Tab from '../components/ui/Tab'
+import SavedAdvice from '../pages/SavedAdvice'
 
 const TabLayout = () => (
   <div className="tab-layout">
@@ -11,7 +12,7 @@ const TabLayout = () => (
     </header>
     <main>
       <Switch>
-        <Route path='/simple-react/saved-advice' />
+        <Route path='/simple-react/saved-advice' component={SavedAdvice} />
         <Route path='/simple-react/user-profile' />
         <Redirect to='/simple-react/saved-advice' />
       </Switch>
---
+++ src/versions/SimpleReact/pages/SavedAdvice.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SavedAdvice = () => (
+  <div className="grid-container">
+    SavedAdvice
+  </div>
+)
+
+export default SavedAdvice
